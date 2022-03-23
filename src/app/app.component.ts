9e8e175eb6923f5beefda6ad8b5f8c0e74de6a28
pravin1160 <impravin4u@gmail.com>
2022-03-23 18:50:13 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  imageUrl: string = "https://cdn.searchenginejournal.com/wp-content/uploads/2019/04/the-seo-guide-to-angular-760x400.png";
+  imageWidth: number = 120;
+  imageHeight: number = 120;
+}
